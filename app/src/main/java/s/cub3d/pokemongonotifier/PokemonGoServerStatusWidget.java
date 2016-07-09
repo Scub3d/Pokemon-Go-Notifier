43e99f77278efb17661d3ac806bd6013464f0651
chinchalupa <wrightjt@rose-hulman.edu>
2016-07-08 21:40:47 -0400

    Added manifest and widget displays

---
+++ app/src/main/java/s/cub3d/pokemongonotifier/PokemonGoServerStatusWidget.java
@@ -0,0 +1,10 @@
+package s.cub3d.pokemongonotifier;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Jeremy on 7/8/2016.
+ */
+public class PokemonGoServerStatusWidget extends AppWidgetProvider {
+
+}
